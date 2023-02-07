595a41090c06baf9eae16152cfc7cfd597455dac
Garland <775320358@qq.com>
2023-02-07 15:23:31 +0800

    ✨ feat: 全局路由守卫

--- src/modules/auth/jwt.strategy.ts
+++ src/modules/auth/jwt.strategy.ts
@@ -2,7 +2,7 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { jwtConstants } from './constants';
-import { Result } from 'src/common/common/dto/result.dto';
+import { User } from '../users/entities/user.entity';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -15,8 +15,12 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   // JWT验证 - Step 4: 被守卫调用
-  async validate() {
+  async validate(payload: User) {
     console.log(`JWT验证 - Step 4: 被守卫调用`);
-    return new Result().ok();
+    return {
+      userName: payload.userName,
+      id: payload.id,
+      realName: payload.realName,
+    };
   }
 }
